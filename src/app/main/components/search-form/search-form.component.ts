c82f8e1ba627306032f21e0937ee59e7751e8d80
AYaki-coder <sj.a.yakimovich@gmail.com>
2023-05-26 22:22:10 +0300

    feat: add passengers block markup

--- src/app/main/components/search-form/search-form.component.ts
+++ src/app/main/components/search-form/search-form.component.ts
@@ -4,63 +4,6 @@ import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { airports } from 'src/constants';
 
-// const airports = [
-//   {
-//     name: 'Hartsfield Jackson Atlanta Intl',
-//     city: 'Atlanta',
-//     country: 'United States',
-//     code: 'ATL',
-//   },
-//   {
-//     name: 'Chicago Ohare Intl',
-//     city: 'Chicago',
-//     country: 'United States',
-//     code: 'ORD',
-//   },
-//   {
-//     name: 'Capital Intl',
-//     city: 'Beijing',
-//     country: 'China',
-//     code: 'PEK',
-//   },
-//   {
-//     name: 'Heathrow',
-//     city: 'London',
-//     country: 'United Kingdom',
-//     code: 'LHR',
-//   },
-//   {
-//     name: 'Charles De Gaulle',
-//     city: 'Paris',
-//     country: 'France',
-//     code: 'CDG',
-//   },
-//   {
-//     name: 'Los Angeles Intl',
-//     city: 'Los Angeles',
-//     country: 'United States',
-//     code: 'LAX',
-//   },
-//   {
-//     name: 'Frankfurt Main',
-//     city: 'Frankfurt',
-//     country: 'Germany',
-//     code: 'FRA',
-//   },
-//   {
-//     name: 'Dallas Fort Worth Intl',
-//     city: 'Dallas-Fort Worth',
-//     country: 'United States',
-//     code: 'DFW',
-//   },
-//   {
-//     name: 'John F Kennedy Intl',
-//     city: 'New York',
-//     country: 'United States',
-//     code: 'JFK',
-//   },
-// ];
-
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
@@ -84,13 +27,27 @@ export class SearchFormComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-
     this.filteredFrom = this.filterAirports(this.from);
     this.filteredTo = this.filterAirports(this.to);
   }
 
-  public buildValue(airport:Airports): string{
+  public buildValue(airport: Airports): string {
     return `${airport.name} (${airport.code})`;
+  }
+
+  public switchAirports(): void {
+    const fromValue = this.from?.value;
+    const toValue = this.to?.value;
+    this.from?.setValue(toValue);
+    this.to?.setValue(fromValue);
+  }
+
+  public onSubmit(): void {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      console.log(1);
+      console.log(this.form);
+    }
   }
 
   private filterAirports(control: AbstractControl | null) {
@@ -109,14 +66,6 @@ export class SearchFormComponent implements OnInit {
     return this.options.filter(({ name, city, country, code }: Airports) =>
       `${name} ${city} ${country} ${code}`.toLowerCase().includes(filterValue),
     );
-  }
-
-  public onSubmit(): void {
-    this.form.markAllAsTouched();
-    if (this.form.valid) {
-      console.log(1);
-      console.log(this.form);
-    }
   }
 }
 
